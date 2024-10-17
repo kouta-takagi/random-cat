b6a387619611cc9b3e5e48c10611ff7171e6bf61
kouta-takagi <k-takagi@ruby-dev.jp>
2024-10-17 15:18:41 +0900

    getServerSideProps実装前

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,40 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+import { NextPage } from "next";
+import { useState, useEffect } from "react";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-);
+const IndexPage: NextPage = () => {
+  const [imageUrl, setImageUrl] = useState("");
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    fetchImage().then((newImage) => {
+      setImageUrl(newImage.url);
+      setLoading(false);
+    });
+  }, []);
+
+  const handleClick = async () => {
+    setLoading(true);
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url);
+    setLoading(false);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>他のニャンコも見るにゃ</button>
+      <div>{loading || <img src={imageUrl} />}</div>
+    </div>
+  );
+};
 export default IndexPage;
+
+type Image = {
+  url: string;
+};
+
+const fetchImage = async (): Promise<Image> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images = await res.json();
+  console.log(images);
+  return images[0];
+};
