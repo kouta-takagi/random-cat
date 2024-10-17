5c203eb3744a49e1744a871bbc70b0ec59167615
kouta-takagi <k-takagi@ruby-dev.jp>
2024-10-17 15:45:04 +0900

    a

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
   return (
     <div className={styles.page}>
       <button onClick={handleClick} className={styles.button}>
-        他のニャンコも見るにゃ
+        他のニャンコも見るッピ
       </button>
       <div className={styles.frame}>
         {loading || <img src={imageUrl} className={styles.img} />}
